c7efadc3657ecf32f2d6a62d65a403a167f577e3
jax-creates <40325536+jax-creates@users.noreply.github.com>
2019-02-11 10:39:05 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE>
+<html>
+<head>
+    <meta name="viewport" content="width = device - width, initial-scale = 1.0">
+    
+    <title>Julian Alvarez || homepage</title>
+    
+    <link rel = "stylesheet" href="style.css" type = "text/css">
+    
+    </head>
+
+    <body>
+        
+        
+        <h1 id="Julian-Alvarez"> <img id = "picture" src="Julian.jpg" ><br>Julian Alvarez</h1>
+        <div class = "links">
+        <a href="contact.php" id = "Contact">Contact</a>
+        <a href="CM.php" id = "CM">Character Models</a>
+        <a href="GameDemo.php" id = "Game demo">Game demonstrations</a>
+        <a href = "OtherWorks.php" id="OW">Other Projects</a>
+            
+        </div>
+        
+    <h2 id="MS-title">Mission Statement</h2>
+        <p id="MS-paragraph"> To obtain a internship to add on to my arsenal of skills to further develop myself as a professional in my field </p>
+    <h2 id = "Bio-title">Biography</h2>
+        <p class="Bio-paragraph">
+        Hebrit Smunti Etiout Erenes Tazina Eelapp Lysagi Tobero Alvarl Eunken
+        Hebrit Smunti Etiout Erenes Tazina Eelapp Lysagi Tobero Alvarl Eunken 
+        Hebrit Smunti Etiout Erenes Tazina Eelapp Lysagi Tobero Alvarl Eunken
+        Hebrit Smunti Etiout Erenes Tazina Eelapp Lysagi Tobero Alvarl Eunken
+        Hebrit Smunti Etiout Erenes Tazina Eelapp Lysagi Tobero Alvarl Eunken
+        Hebrit Smunti Etiout Erenes Tazina Eelapp Lysagi Tobero Alvarl Eunken
+        </p>
+    <h2 id="Resume-title">Resume</h2>
+        <p id="Resume-paragraph"><img src = "Julian%20Alvarez%C2%A0Resume%202018%20(1)_000.jpg" height="650px" width="600px"></p>
+    
+    </body>
+</html>
